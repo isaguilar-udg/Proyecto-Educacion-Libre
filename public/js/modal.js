81710e2fc2355b419d50447b3ad1b5ed5a7bbfb6
isaguilar-udg <isela.aguilar23b@udgvirtual.udg.mx>
2024-03-16 19:02:52 -0600

    Código de página EduLibre

---
+++ public/js/modal.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var modal = document.getElementById("modal");
+
+    var button = document.getElementById("openModal");
+    var anchor = document.getElementById("openModal2");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    button.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+
+    anchor.addEventListener("click", (event) => {
+        event.preventDefault();
+        modal.style.display = "block";
+    })
+
+    span.addEventListener('click', (event) => {
+        modal.style.display = "none";
+    })
+});
